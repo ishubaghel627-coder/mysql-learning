206d69fe5813ff46634ca14210142fa81b270070
ishubaghel627-coder <ishubaghel627@gmail.com>
2026-04-16 17:45:05 +0530

    Delete GROUP_BY&ORDER_BY.sql3.sql

--- GROUP_BY&ORDER_BY.sql3.sql
+++
@@ -1,7 +0,0 @@
-SELECT gender,AVG(age),MAX(age),MIN(age),COUNT(age)
-FROM parks_and_recreation.employee_demographics
-GROUP BY gender;
-
-SELECT *
-FROM parks_and_recreation.employee_demographics
-ORDER BY gender,age;
