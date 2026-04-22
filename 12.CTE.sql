7d4b91beb5054dcf67c1990516099464a988a956
ishubaghel627-coder <ishubaghel627@gmail.com>
2026-04-23 01:30:54 +0530

    Add files via upload

---
+++ 12.CTE.sql
@@ -0,0 +1,29 @@
+WITH CTE_Example AS
+(
+SELECT gender, AVG(salary) avg_sal,MAX(salary) max_sal,MIN(salary) min_sal,COUNT(salary) count_sal
+FROM parks_and_recreation.employee_demographics dem
+JOIN parks_and_recreation.employee_salary sal
+   ON dem.employee_id=sal.employee_id
+GROUP BY gender
+)
+SELECT AVG(avg_sal)
+FROM CTE_Example
+;
+ 
+ WITH CTE_Example AS
+ (
+ SELECT employee_id,gender,birth_date
+ FROM parks_and_recreation.employee_demographics
+ WHERE birth_date>'1985-01-01'
+ ),
+ CTE_Example2 AS
+ (
+ SELECT employee_id,salary
+ FROM parks_and_recreation.employee_salary
+ WHERE salary>50000
+ )
+ SELECT *
+ FROM CTE_Example
+ JOIN CTE_Example2
+	ON CTE_Example.employee_id=CTE_Example2.employee_id
+ ;   
