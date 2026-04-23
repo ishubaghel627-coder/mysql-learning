d17fd7938f811b1da83cfaab308b20b4e15e75bd
ishubaghel627-coder <ishubaghel627@gmail.com>
2026-04-24 00:56:32 +0530

    Add files via upload

---
+++ 14.STORE_PROCEDURE.sql
@@ -0,0 +1,31 @@
+CREATE PROCEDURE large_salaries()
+SELECT *
+FROM employee_salary
+WHERE salary>=50000;
+
+CALL large_salaries();
+
+DELIMITER $$
+CREATE PROCEDURE large_salaries3()
+BEGIN 
+    SELECT *
+    FROM employee_salary
+    WHERE salary>=50000;
+    SELECT *
+    FROM employee_salary
+    WHERE salary>=10000;
+END $$
+DELIMITER ; 
+
+CALL large_salaries3();  
+
+DELIMITER $$
+CREATE PROCEDURE large_salaries4(x INT)
+BEGIN 
+    SELECT salary
+    FROM employee_salary
+    WHERE employee_id=x;
+END $$
+DELIMITER ; 
+
+CALL large_salaries4(1);  
